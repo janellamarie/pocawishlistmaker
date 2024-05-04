865272b3bd1f13022c8f976bbd27bde5edaf8673
Janella <fullmoonfirefly11@gmail.com>
2024-05-04 19:32:46 -0400

    Update models.py

--- pocawishlistmaker/models.py
+++ pocawishlistmaker/models.py
@@ -1,21 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 class Items(models.Model):
   id = models.AutoField(primary_key=True)
   name = models.CharField()
   price = models.IntegerField()
-  created_at = models.DateTimeField()
+  created_at = models.DateTimeField(default=timezone.now())
   updated_at = models.DateTimeField()
+
+  def __str__(self):
+    return self.id, ": ", self.name, "(", self.price, ")"
 
 class Tags(models.Model):
   id = models.AutoField(primary_key=True) 
   tag = models.CharField()
-  created_at = models.DateTimeField()
+  created_at = models.DateTimeField(default=timezone.now())
   updated_at = models.DateTimeField()
+
+  def __str__(self):
+    return self.id, ": ", self.tag
 
 class Wishlists(models.Model):
   id = models.AutoField(primary_key=True)
   name = models.CharField()
   description = models.CharField()
-  created_at = models.DateTimeField()
+  created_at = models.DateTimeField(default=timezone.now())
   updated_at = models.DateTimeField()
+
+  def __str__(self):
+    return self.id, ": ", self.name
