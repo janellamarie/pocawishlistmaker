3dee191259e2b4c1a97787558ed973b23f1165d2
Janella <fullmoonfirefly11@gmail.com>
2024-05-08 01:19:18 -0400

    Add base for navigation bar

--- view/src/App.js
+++ view/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Grid, GridItem} from '@chakra-ui/react'
+
+function Navigation() {
+  return(
+    <Grid templateColumns='repeat(5, 1fr)' data-testid='navigation-bar'>
+      <GridItem w='100%'>
+        <h1>Poca Wishlist Maker</h1>
+      </GridItem>
+      <GridItem colStart={4}>
+        <Button>
+          Items
+        </Button>
+      </GridItem>
+      <GridItem>
+        <Button>
+          Wishlists
+        </Button>
+      </GridItem>
+    </Grid>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid templateAreas={`"header header"
+                          "nav main"
+                          "nav footer"`}>
+      <GridItem>
+        <Navigation />
+      </GridItem>
+    </Grid>
   );
 }
 
--- view/src/App.test.js
+++ view/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders navigation bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const navbar = screen.get("[data-testid='navigation-bar']");
+  expect(navbar).toBeInTheDocument();
 });
