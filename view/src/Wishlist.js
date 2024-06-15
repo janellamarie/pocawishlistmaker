ddb61770d48b200a1b69ae6baca4a3bd4aa49dee
Janella <fullmoonfirefly11@gmail.com>
2024-06-15 19:41:59 -0400

    Add base UI

--- view/src/Wishlist.js
+++ view/src/Wishlist.js
@@ -1,3 +1,32 @@
+import { SearchIcon } from "@chakra-ui/icons"
+import { Box, Divider, Grid, GridItem, Heading, IconButton, Input, InputGroup, InputRightElement, VStack } from "@chakra-ui/react"
+
 export function Wishlists() {
-  
+  return (
+    <Grid templateAreas={`"list main"`}
+      gridTemplateColumns={'30% 1fr'}
+    >
+      <GridItem area={'list'}>
+        <Box backgroundColor='blue.200'>
+          <Heading>Wishlists</Heading>
+          <InputGroup>
+            <Input placeholder='Search' variant='filled' />
+            <InputRightElement>
+              <IconButton icon={<SearchIcon />} size='sm'/>
+            </InputRightElement>
+          </InputGroup>
+        </Box>
+        <Divider/>
+        <VStack>
+          <Box>
+            
+          </Box>
+        </VStack>
+        
+      </GridItem>
+      <GridItem area={'main'}>
+
+      </GridItem>
+    </Grid>
+  )
 }
